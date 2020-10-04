1aa2131a98f203ae95ceea03a7d008943fdae622
Aurel PIERRE <aurel.pierre.pro@gmail.com>
2020-10-04 21:19:02 +0200

    Add Mesh

--- EzUtils/include/VkRenderer/Mesh.h
+++ EzUtils/include/VkRenderer/Mesh.h
@@ -4,6 +4,8 @@
 
 #include <vector>
 
+#include "Context.h"
+#include "Device.h"
 #include "Material.h"
 
 struct Mesh
@@ -14,8 +16,11 @@ struct Mesh
 		{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
 	};
 
-	Material			_material = VK_NULL_HANDLE;
+	Material*			_material	= nullptr;
 
-	VkBuffer			_buffer = VK_NULL_HANDLE;
-	VkDeviceMemory		_memory = VK_NULL_HANDLE;
+	VkBuffer			_buffer		= VK_NULL_HANDLE;
+	VkDeviceMemory		_memory		= VK_NULL_HANDLE;
 };
+
+Mesh	CreateMesh(const Context& kContext, const LogicalDevice& kLogicalDevice, const Device& kDevice);
+void	DestroyMesh(const Context& kContext, const LogicalDevice& kLogicalDevice, const Mesh& kMesh);
---
+++ EzUtils/src/VkRenderer/Mesh.cpp
@@ -0,0 +1,46 @@
+#include "Mesh.h"
+
+
+Mesh	CreateMesh(const Context& kContext, const LogicalDevice& kLogicalDevice, const Device& kDevice)
+{
+	Mesh mesh{};
+
+	VkBufferCreateInfo bufferInfo{};
+	bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	bufferInfo.size = sizeof(mesh.vertices[0]) * mesh.vertices.size();
+	bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+	bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+	if (vkCreateBuffer(kLogicalDevice._device, &bufferInfo, kContext._allocator, &mesh._buffer) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create vertex buffer!");
+	}
+
+	VkMemoryRequirements memRequirements;
+	vkGetBufferMemoryRequirements(kLogicalDevice._device, mesh._buffer, &memRequirements);
+
+	uint32_t type = UINT32_MAX;
+	for (uint32_t i = 0; i < kDevice._memoryProperties.memoryTypeCount; i++) {
+		if ((memRequirements.memoryTypeBits & (1 << i)) && (kDevice._memoryProperties.memoryTypes[i].propertyFlags & (VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)) == (VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)) {
+			type = i;
+		}
+	}
+	if (type == UINT32_MAX)
+		throw;
+
+	VkMemoryAllocateInfo allocInfo{};
+	allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	allocInfo.allocationSize = memRequirements.size;
+	allocInfo.memoryTypeIndex = type;
+
+	if (vkAllocateMemory(kLogicalDevice._device, &allocInfo, kContext._allocator, &mesh._memory) != VK_SUCCESS) {
+		throw std::runtime_error("failed to allocate vertex buffer memory!");
+	}
+
+	vkBindBufferMemory(kLogicalDevice._device, mesh._buffer, mesh._memory, 0);
+}
+
+void	DestroyMesh(const Context& kContext, const LogicalDevice& kLogicalDevice, const Mesh& kMesh)
+{
+	vkFreeMemory(kLogicalDevice._device, kMesh._memory, kContext._allocator);
+	vkDestroyBuffer(kLogicalDevice._device, kMesh._buffer, kContext._allocator);
+}
