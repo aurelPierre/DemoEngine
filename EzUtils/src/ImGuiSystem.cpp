f2779940d3eda410ee9870d31f501f3fcc49e7d4
Aurel PIERRE <aurel.pierre.pro@gmail.com>
2020-10-07 23:18:41 +0200

    Update imgui default docking windows

--- EzUtils/src/ImGuiSystem.cpp
+++ EzUtils/src/ImGuiSystem.cpp
@@ -151,6 +151,23 @@ void ImGuiSystem::StartFrame()
 	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
 	{
 		ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+		if (ImGui::DockBuilderGetNode(dockspace_id) == NULL)
+		{
+			ImGui::DockBuilderRemoveNode(dockspace_id); // Clear out existing layout
+			ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_DockSpace); // Add empty node
+			ImGui::DockBuilderSetNodeSize(dockspace_id, ImGui::GetWindowSize());
+
+			ImGuiID dock_main_id = dockspace_id; // This variable will track the document node, however we are not using it here as we aren't docking anything into it.
+			ImGuiID dock_id_left = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Left, 0.20f, NULL, &dock_main_id);
+			ImGuiID dock_id_right = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Right, 0.20f, NULL, &dock_main_id);
+			ImGuiID dock_id_bottom = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Down, 0.20f, NULL, &dock_main_id);
+
+			ImGui::DockBuilderDockWindow("Logs", dock_id_right);
+			ImGui::DockBuilderDockWindow("Scene", dock_id_right); 
+			ImGui::DockBuilderDockWindow("Profiler", dock_id_right);
+			ImGui::DockBuilderDockWindow("Viewport", dock_main_id);
+			ImGui::DockBuilderFinish(dockspace_id);
+		}
 		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
 	}
 
