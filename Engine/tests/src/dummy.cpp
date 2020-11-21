6d647030273c7db194cd342ba145ed3965c7f866
Aurel PIERRE <aurel.pierre.pro@gmail.com>
2020-11-21 15:42:28 +0100

    add assert in dummy test

--- Engine/tests/src/dummy.cpp
+++ Engine/tests/src/dummy.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 
+#include "VkRenderer/Core.h"
 #include "GLFWWindowSystem.h"
 
 int main(int, char**)
@@ -7,6 +8,7 @@ int main(int, char**)
 	{
 		GLFWWindowSystem glfwWindow;
 		GLFWWindowData* windowData = glfwWindow.CreateWindow();
+		ASSERT(windowData != nullptr, "window data is nullptr")
 		glfwWindow.DeleteWindow();
 	}
 
