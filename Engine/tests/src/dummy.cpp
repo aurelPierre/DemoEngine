bbab779a601faaae1238fb2b65146d007e920dbe
Aurel PIERRE <aurel.pierre.pro@gmail.com>
2020-11-22 14:16:56 +0100

    update dummy test with vulkan

--- Engine/tests/src/dummy.cpp
+++ Engine/tests/src/dummy.cpp
@@ -3,6 +3,9 @@
 #include "VkRenderer/Core.h"
 #include "GLFWWindowSystem.h"
 
+#include "VkRenderer/Context.h"
+#include "VkRenderer/Swapchain.h"
+
 int main(int, char**)
 {
 	ez::LogSystem::_standardOutput = true;
@@ -11,6 +14,15 @@ int main(int, char**)
 		GLFWWindowSystem glfwWindow;
 		GLFWWindowData* windowData = glfwWindow.CreateWindow();
 		ASSERT(windowData != nullptr, "window data is nullptr")
+
+		Context context;
+		Device device;
+		LogicalDevice logicalDevice(device);
+		Surface surface(windowData);
+		Swapchain swapchain(surface, windowData);
+
+		vkDeviceWaitIdle(logicalDevice._device);
+
 		glfwWindow.DeleteWindow();
 	}
 
